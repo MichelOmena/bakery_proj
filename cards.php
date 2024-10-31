e30010b0e2edc3bd207d5f260eacb9ec884349ed
Michel Omena <micheltenoriodeomena@hotmail.com>
2024-10-31 05:51:43 +0000

    updates php files

---
+++ cards.php
@@ -0,0 +1,27 @@
+<?php
+function getCards() {
+    return [
+        //1º card
+        [
+            'imagem' => 'src/img/pastel_nata.png',
+            'titulo' => 'Pastel de Nata Recheada',
+            'descricao' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
+            'avaliacao' => 5,
+            'avaliacao_total' => ('500+'),
+            'preco' => '2,00'
+        ],
+
+        //2º card
+        [
+            'imagem' => 'src/img/pastel_nata.png',
+            'titulo' => 'Pastel de Nata Tradicional',
+            'descricao' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
+            'avaliacao' => 4,
+            'avaliacao_total' => 300,
+            'preco' => '1,50'
+        ]
+
+
+        ];
+}
+?>
--- index.php
+++ index.php
@@ -1,18 +1,26 @@
+<?php
+include 'cards.php';
+//function
+$cards = getCards();
+?>
+
 <!DOCTYPE html>
 <html lang="pt">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="shortcut icon" href="./src/img/favicon.ico" type="image/x-icon">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" />
     <link rel="stylesheet" href="./src/styles/styles.css">
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js" integrity="sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <title>Crisnuel</title>
 </head>
+
 <body>
     <header>
         <!--navBar-->
-        <nav id="navbar">   
+        <nav id="navbar">
             <i class="fa-solid fa-store" id="nav_logo">Padaria & Pastelaria</i>
 
             <ul id="nav_list">
@@ -29,7 +37,7 @@
 
             <!--Botao padrao-->
             <button class="btn-default">
-                Encomende já, clique aqui
+                Encomende aqui
             </button>
 
             <button id="mobile_btn">
@@ -59,25 +67,26 @@
 
     <main id="content">
         <section id="home">
+            <div class="shape"></div>
             <div id="cta">
                 <h1 class="title">
-                    O sabor vai até
-                    <span>você</span>
+                    O calor do forno, o sabor da
+                    <span>tradição</span>
                 </h1>
 
                 <p class="description">
-                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Commodi velit saepe hic ratione quasi minus autem at soluta, cupiditate dignissimos reprehenderit, explicabo ea amet accusantium inventore sint id nemo reiciendis!
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit.
                 </p>
-                <div class="cta_buttons">
+                <div id="cta_buttons">
                     <a href="#" class="btn-default">
-                        Nossas especialidades 
+                        Produtos
                     </a>
 
                     <a href="tel:+11111111111" id="phone_button">
                         <button class="btn-default">
                             <i class="fa-solid fa-phone"></i>
-                            2376 2736263
                         </button>
+                        2376 2736263
                     </a>
                 </div>
 
@@ -101,12 +110,59 @@
                 </div>
             </div>
 
-            <div class="banner">
-
+            <div id="banner">
+                <img src="src/img/bread_hero.png" alt="">
             </div>
         </section>
     </main>
+
+    <section id="menu">
+        <h2 class="section-title">Mais vendidos</h2>
+        <h3 class="section-subtitle">Nossas especialidades</h3>
+
+        <div class="container">
+            <!--loop for dishes-->
+            <?php foreach ($cards as $card): ?>
+            <div id="dishes">
+                <div class="dish">
+                    <div class="dish-heart">
+                        <i class="fa-solid fa-heart"></i>
+                    </div>
+
+                    <img src="<?php echo $card['imagem']; ?>" alt="<?php echo $card['titulo']; ?>" class="w-25 img-auto">
+
+                    <h3 class="dish-title">
+                       <?php echo $card['titulo']; ?>
+                    </h3>
+                    <span class="dish-description">
+                        <?php echo $card['descricao']; ?>
+                    </span>
+
+                    <div class="dish-rate">
+                        <?php for ($i = 0; $i < $card['avaliacao']; $i++): ?>
+                        <i class="fa-solid fa-star"></i>
+                        <?php endfor; ?>
+                        
+                        <span>(<?php echo $card['avaliacao_total']; ?>+)</span>
+                    </div>
+
+                    <div class="dish-price">
+                        <h4><?php echo $card['preco']; ?> €</h4>
+                        <button class="btn-default">
+                            <i class="fa-solid fa-basket-shopping"></i>
+                        </button>
+                    </div>
+                </div>
+            </div>
+            <?php endforeach; ?>
+        </div>
+    </section>
     <!--Script JS-->
     <script src="./src/javascript/scripts.js"></script>
+    <!--Js Bootstrap-->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <!-- jQuery -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js" integrity="sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 </body>
+
 </html>
